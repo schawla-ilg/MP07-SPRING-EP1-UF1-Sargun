750e91a24c7b4a097802e9973169d552f829ebe6
Sargun Chawla <schawla@ilg.cat>
2022-02-06 21:02:11 +0000

    Nivell2

---
+++ Nivell2/Activitat1/src/main/java/cat/ilg/daw/m7/uf1/Activitat1/Activitat1Application.java
@@ -0,0 +1,15 @@
+package cat.ilg.daw.m7.uf1.Activitat1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activitat1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Activitat1Application.class, args);
+		SpringApplication.run(cat.ilg.daw.m7.uf1.Activitat1.ControlerNivell2.class, args);
+	}
+
+}
---
+++ Nivell2/Activitat1/src/test/java/cat/ilg/daw/m7/uf1/Activitat1/Activitat1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.ilg.daw.m7.uf1.Activitat1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activitat1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
